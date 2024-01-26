0001840c05dd0ea61a61c28a92a67a96def8cbb6
Kgaugelo Ramaramela <kgaugelo.ramaramela@standardbank.co.za>
2024-01-26 20:50:58 +0200

    re-writes music endpoint to use Webflux

--- src/main/kotlin/com/sage/sage/microservices/music/controller/MusicController.kt
+++ src/main/kotlin/com/sage/sage/microservices/music/controller/MusicController.kt
@@ -1,10 +1,7 @@
 package com.sage.sage.microservices.music.controller
 
-import com.sage.sage.microservices.music.model.request.AlbumModel
-import com.sage.sage.microservices.music.model.request.TrackModel
-import com.sage.sage.microservices.music.model.request.TrackModel2
+import com.sage.sage.microservices.music.model.request.*
 import com.sage.sage.microservices.music.service.MusicService
-import com.sage.sage.microservices.music.model.request.TrackResponse
 import com.sage.sage.microservices.music.model.response.SearchAlbumsResponse
 import com.sage.sage.microservices.music.model.response.SearchTracksResponse
 import org.springframework.http.ResponseEntity
@@ -14,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/music")
@@ -21,27 +20,27 @@ class MusicController(private val musicService: MusicService) {
 
 
     @GetMapping("/track/{trackId}")
-    fun getTrack(@PathVariable trackId: String): ResponseEntity<TrackModel2> {
+    fun getTrack(@PathVariable trackId: String): Mono<TrackModel2> {
         return musicService.getTrack(trackId)
     }
 
     @PostMapping("album/add")
-    fun createAlbum(@RequestBody albumRequest: AlbumModel): ResponseEntity<String>{
+    fun createAlbum(@RequestBody albumRequest: AlbumModel): Mono<Void>{
        return musicService.createAlbum(albumRequest)
     }
 
     @GetMapping("/album/{albumId}")
-    fun getAlbum(@PathVariable albumId: String): ResponseEntity<AlbumModel?> {
+    fun getAlbum(@PathVariable albumId: String): Mono<AlbumModel> {
         return musicService.getAlbum(albumId)
     }
 
     @GetMapping("album/search/{query}")
-    fun searchAlbums(@PathVariable query: String): ResponseEntity<SearchAlbumsResponse>{
+    fun searchAlbums(@PathVariable query: String): Flux<AlbumModel2>{
         return musicService.searchAlbums(query)
     }
 
     @GetMapping("track/search/{query}")
-    fun searchTracks(@PathVariable query: String): ResponseEntity<SearchTracksResponse>{
+    fun searchTracks(@PathVariable query: String): Flux<TrackModel2>{
         return musicService.searchTrack(query)
     }
 
