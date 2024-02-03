913a32546a0cac463cfb24ede84d4021ea7ef6b1
Kgaugelo Ramaramela <kgaugelo.ramaramela@standardbank.co.za>
2024-02-03 20:33:00 +0200

    test codecov covarage

---
+++ src/main/kotlin/com/sage/sage/microservices/coroutines/TheFileOfLegends.kt
@@ -0,0 +1,20 @@
+package com.sage.sage.microservices.coroutines
+
+class TheFileOfLegends {
+
+    fun addition(a: Int, b:Int): Int{
+        return a + b
+    }
+
+    fun subtraction(a: Int, b:Int): Int{
+        return a - b
+    }
+
+    fun multiplication(a: Int, b:Int): Int{
+        return a * b
+    }
+
+    fun division(a: Int, b:Int): Int{
+        return a / b
+    }
+}
